a0513b1af40e65cbfd86c5f46676aece4d1e5f3d
Pawel Kapala <github@kapware.com>
2019-05-04 18:33:20 +0200

    Verified correctnes on a generated set

--- src/scramble/core.clj
+++ src/scramble/core.clj
@@ -1,11 +1,24 @@
-(ns core)
+(ns core
+  (:require [clojure.spec.alpha :as s]))
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+
+(s/def ::scramblie (s/with-gen
+                     (s/and string?
+                            #(re-matches #"[a-z]*" %))
+                     #(gen/fmap
+                       (fn [chars] (apply str (map char chars)))
+                       (gen/vector (gen/elements (char-range \a \z))))))
+
+  
 (defn fmap [f m]
   (into (empty m) (for [[k v] m] [k (f v)])))
 
 
 (defn scramble? [letters a-word]
-  (if (> (count letters) (count a-word))
+  (if (>= (count letters) (count a-word))
     (not-any? neg?
               (vals
                (merge-with +
@@ -50,4 +63,37 @@
                      (frequencies "rekqodlw") 
                      (fmap (partial * -1)
                            (frequencies "world")) )) )  
+
+  ;; Verify scramble? arg(s) domain
+  (s/explain-str ::scramblie "AAA") 
+  (s/explain-str ::scramblie "aaaa") 
+  (s/explain-str ::scramblie "a.!") 
+  (s/explain-str ::scramblie 1) 
+
+  (clojure.string/lower-case "FOO") 
+  (require '[clojure.test.check.generators :as gen]) 
+  ;; Let's generate some scramblies reliably
+  ;; ... but first, we need a proper generator
+  (gen/sample (gen/vector (gen/elements (char-range \a \z))))
+  (gen/sample (s/gen ::scramblie) 1000) 
+
+  ;; Now we should be ready generate some test data for our function:
+  ;; generate a pair of scramblies -> add some noise and shuffle
+  (apply str (shuffle (seq (str "foo" "bar")))) 
+  (def test-data (gen/sample (gen/fmap
+                              (fn [[scramblie noise]]
+                                [(apply str ((fnil shuffle []) (seq (str scramblie noise))))
+                                 scramblie])
+                              (gen/tuple
+                               (s/gen ::scramblie)
+                               (s/gen ::scramblie))) 1024)) 
+  ;; Bump:
+  (shuffle nil) ;; => NPE
+  ;; solution:
+  ((fnil shuffle []) nil) ;; => happy :)
+
+  (filter (fn [[_ _ result]] (false? result))
+          (map (fn [[letters word]]
+                 [letters word (scramble? letters word)])
+                   test-data))  
 )
